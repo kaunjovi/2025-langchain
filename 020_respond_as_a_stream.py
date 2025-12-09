59cc34ddaaa0236d6d80cd485de039aa7c1a7899
kaunjovi <kaunjovi@gmail.com>
2025-12-09 10:46:11 +0530

    making ruff a part of the build.

--- 020_respond_as_a_stream.py
+++ 020_respond_as_a_stream.py
@@ -4,17 +4,18 @@ OLLAMA_BASE_URL = "http://localhost:11434"
 MODEL = "deepseek-r1:8b"
 
 
-def respond_as_a_stream(city: str = "Delhi") -> None:
-    llm = ChatOllama(model=MODEL, base_url=OLLAMA_BASE_URL)
+def respond_as_a_stream(city: str = "Jaipur") -> None:
+    llm = ChatOllama(model=MODEL, base_url=OLLAMA_BASE_URL, reasoning=True)
 
     messages = [
         (
             "system",
-            "You are a local guide to India. Help create visitors a detailed itinerary for their trip based on their preferences.",
+            "You are a local guide to India. Help create visitors a detailed "
+            "itinerary for their trip based on their preferences.",
         ),
         (
             "human",
-            f"I am planning a trip to {city}. Can you suggest an itinerary for me please",
+            f"I am planning a trip to {city}. Can you suggest an itinerary for me please, for a 2 day trip?",
         ),
     ]
 
